434c1d4d16edfb9de3ec40a658b1c7798d6a338b
lastgamerrz <101456911+lastgamerrz@users.noreply.github.com>
2024-06-29 13:09:01 +0000

    C

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #installing ngrok
-#curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
 
 #adding authtoken
-#ngrok authtoken 2Xf9VS1X9J8Cm0fDUD0WGUbGm4a_6YSNePWcAamMrBHixXB2X
+ngrok authtoken 2Xf9VS1X9J8Cm0fDUD0WGUbGm4a_6YSNePWcAamMrBHixXB2X
 
 #starting the server
-./server.sh &
+./server.sh 
 
-sleep 20
+sleep 60
 #make ngrok tunnel
-#ngrok tcp 25565
+ngrok tcp 25565
--- stop.sh
+++ stop.sh
@@ -1,24 +1,6 @@
-#!/bin/bash
 
-# Navigate to the directory where the script is located
-cd "$(dirname "$0")"
-
-# Add all changes to the staging area
 git add .
 
-# Get the current date and format it
-current_date=$(date "+%Y-%m-%d %H:%M:%S")
+git commit -m "C"
 
-# Commit changes with the current date as part of the commit message
-git commit -m "Commit on $current_date"
-
-# Push changes to the remote repository (replace 'main' with your branch name)
 git push origin main
-
-# Check the exit status of the git commands
-if [ $? -ne 0 ]; then
-    echo "Error: Git operation failed"
-    exit 1
-else
-    echo "Git operations completed successfully"
-fi
